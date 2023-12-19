bb5a92ccdc896bc29d05c17cee10f9c1dccdb7dc
matp1113 <mateusz.patora@led-pol.pl>
2023-12-19 21:17:05 +0100

    resolved issues, now scripts are working

--- script.sh
+++ script.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/bash
+
+path=$PWD
 sudo apt-get install git
 sudo apt-get install cmake
 mkdir DJI
 cd DJI
 git clone https://github.com/dji-sdk/Payload-SDK.git
-cd Payload-SDK/samples/sample_c/module_sample/
+cd $path/DJI/Payload-SDK/samples/sample_c/module_sample/
 rm -r fc_subscription/
-cd ~/DJI/
+cd $path/DJI/
 git clone https://github.com/11mat13/fc_subscription.git
-cp -r fc_subscription/fc_subscription/ Payload-SDK/samples/sample_c/module_sample/
-cd Payload-SDK/samples/sample_c/platform/linux/manifold2/
+cp -r $path/DJI/fc_subscription/fc_subscription/ $path/DJI/Payload-SDK/samples/sample_c/module_sample/
+cd $path/DJI/Payload-SDK/samples/sample_c/platform/linux/manifold2/
 rm CMakeLists.txt
-cd ~/DJI/
-cp fc_subsrciption/CMakeLists.txt Payload-SDK/samples/sample_c/platform/linux/manifold2/
+cd $path/DJI/
+cp fc_subscription/CMakeLists.txt Payload-SDK/samples/sample_c/platform/linux/manifold2/
 
 
--- script2.sh
+++ script2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-
-cd ~/DJI/Payload-SDK/samples/sample_c/platform/linux/manifold2/
+path = $PWD
+cd path/DJI/Payload-SDK/samples/sample_c/platform/linux/manifold2/
 mkdir project/
 cd project/
 cmake..
