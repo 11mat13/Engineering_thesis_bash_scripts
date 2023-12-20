9045e5312496587812d455245b951d7821425c80
matp1113 <mateusz.patora@led-pol.pl>
2023-12-20 14:12:09 +0100

    modified script.sh

--- script.sh
+++ script.sh
@@ -13,7 +13,14 @@ git clone https://github.com/11mat13/fc_subscription.git
 cp -r $path/DJI/fc_subscription/fc_subscription/ $path/DJI/Payload-SDK/samples/sample_c/module_sample/
 cd $path/DJI/Payload-SDK/samples/sample_c/platform/linux/manifold2/
 rm CMakeLists.txt
+cd application/
+rm dji_sdk_config.h
 cd $path/DJI/
 cp fc_subscription/CMakeLists.txt Payload-SDK/samples/sample_c/platform/linux/manifold2/
+cp fc_subscription/version.h Payload-SDK/samples/sample_c/module_sample/
+cp fc_subscription/dji_sdk_config.h Payload-SDK/samples/sample_c/platform/linux/manifold2/application/
+cd ~/
+sudo echo -e "\nsudo insmod soft_uart.ko gpio_tx=27 gpio_rx=22" | sudo tee -a /etc/rc.local > /dev/null
+sudo shutdown -r now
 
 
