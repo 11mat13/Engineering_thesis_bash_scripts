5b69eef79de66f17a41fa968a8fc993ef6b4f92e
matp1113 <mateusz.patora@led-pol.pl>
2023-12-20 14:23:52 +0100

    added script3.sh

---
+++ script3.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+cd ~/DJI/
+git clone https://github.com/adrianomarto/soft_uart
+git clone https://github.com/11mat13/termios.git
+sudo apt-get install raspberrypi-kernel-headers
+cd soft_uart
+rm module.c
+cd ..
+cp termios/module.c soft_uart/
+cd soft_uart/
+make
+sudo make install
+
+
